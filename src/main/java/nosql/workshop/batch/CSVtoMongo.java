db1ccf897489eaa128a2338c2b8013a0f6322e27
theophileMorin <theophile.morin.pro@gmail.com>
2016-02-09 08:57:20 +0100

    Updated to DBObjects

--- src/main/java/nosql/workshop/batch/CSVtoMongo.java
+++ src/main/java/nosql/workshop/batch/CSVtoMongo.java
@@ -1,16 +1,19 @@
 package nosql.workshop.batch;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import nosql.workshop.model.Adresse;
 import nosql.workshop.model.Equipement;
 import nosql.workshop.model.Installation;
 import nosql.workshop.model.Location;
-import nosql.workshop.services.MongoDB;
-import org.jongo.Jongo;
-import org.jongo.MongoCollection;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -24,20 +27,17 @@ public class CSVtoMongo {
     public static void main(String[] args) throws FileNotFoundException, UnknownHostException{
         System.out.println("Status: Trying to connect to MongoDB...");
 
-        //Retrieving Jongo instance to use MongoDB.
-        MongoDB db = new MongoDB();
         long beg, end;
-        Jongo jongo = db.getJongo();
-        MongoCollection installations = jongo.getCollection("installations");
-
+        MongoCollection installations = getCollection();
         List<Installation> installationList = readInstallationCSV();
+
 
         System.out.println("Status: Trying to insert data into db...");
         try {
             beg = System.currentTimeMillis();
 
             installations.drop();
-            installations.insert(installationList.toArray()); //Bulk insert
+            //installations.insert(installationList.toArray()); //Bulk insert
 
             end = System.currentTimeMillis();
             System.out.println("Status: data inserted in " + (end - beg)+ "ms.");
@@ -49,6 +49,18 @@ public class CSVtoMongo {
         //TODO add activities;
 
         System.out.println("Job done.");
+    }
+
+    private static MongoCollection getCollection() {
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("nosql-workshop");
+        MongoCollection installations = db.getCollection("installations");
+        return installations;
+
+        //Or with Jongo
+        /*MongoDB db = new MongoDB();
+        Jongo jongo = db.getJongo();
+        MongoCollection installations = jongo.getCollection("installations");*/
     }
 
     private static void addEquipementDataFromCSV(MongoCollection collection)
@@ -164,13 +176,13 @@ public class CSVtoMongo {
 
         String ref = instData[2];
 
-        Installation current = collection.findOne("{_id : #}", ref).as(Installation.class);
+        /*Installation current = collection.findOne("{_id : #}", ref).as(Installation.class);
         Equipement[] news = new Equipement[current.equipements.length+1];
         System.arraycopy(current.equipements,0, news, 0, current.equipements.length);
         news[current.equipements.length] = eq;
         current.equipements = news;
 
-        collection.save(current);
+        collection.save(current);*/
         // or
         //collection.update("{_id : #}", ref).with(current);
     }
@@ -212,6 +224,7 @@ public class CSVtoMongo {
     }
 
 
+
     private static Equipement mapCSVwithEquipmentPojo(String[] csvData) {
         Equipement eq = null;
 
@@ -241,4 +254,66 @@ public class CSVtoMongo {
         }
         return eq;
     }
+
+
+
+
+    private static DBObject mapInstallationCSVtoDBObject(String[] csvData) {
+        try {
+            DBObject addr = new BasicDBObject("commune", csvData[2])
+                    .append("codePostal", "".equals(csvData[4])? 0 : Integer.parseInt(csvData[4]))
+                    .append("numero", csvData[6])
+                    .append("voie", csvData[7])
+                    .append("lieuDit",  csvData[5]);
+
+            DBObject loc = new BasicDBObject("type", "Point")
+                    .append("coordinates", Arrays.asList(Float.parseFloat(csvData[9]),Float.parseFloat(csvData[10])));
+
+            DBObject inst = new BasicDBObject("_id", csvData[1])
+                    .append("nom", csvData[0].substring(1))
+                    .append("adresse", addr)
+                    .append("multiCommune", "oui".equalsIgnoreCase(csvData[16]))
+                    .append("nbPlacesParking", "".equals(csvData[17]) ? 0 : Integer.parseInt(csvData[17]))
+                    .append("nbPlacesParkingHandicapes", "".equals(csvData[18]) ? 0 : Integer.parseInt(csvData[18]))
+                    .append("dateMiseAJourFiche", csvData[28].substring(0, csvData[28].length() - 2))
+                    .append("location", loc)
+                    .append("equipements", Arrays.asList());
+
+            return inst;
+        }catch(Exception e) {
+            System.err.println(e.toString());
+            return null;
+        }
+    }
+
+
+    private static DBObject mapEquipementCSVtoDBObject(String[] csvData) {
+        try {
+            int offset = 0;
+            Integer numero = null;
+            try {
+                numero = Integer.parseInt(csvData[4]);
+            }catch(Exception e) {
+                System.err.println(e.toString());
+                while(!csvData[4+offset].substring(csvData[4+offset].length()-1, csvData[4+offset].length()).equals("\"")) {
+                    ++offset;
+                    System.err.println("Retrying for: " + csvData[4+offset]);
+                }
+                ++offset;
+                System.err.println("Retrying for: " + csvData[4+offset]);
+                numero = Integer.parseInt(csvData[4+offset]);
+            }
+            DBObject equip = new BasicDBObject("numero", numero)
+                    .append("nom", csvData[5+offset])
+                    .append("type", csvData[7+offset])
+                    .append("famille", csvData[9+offset])
+                    .append("activites", Arrays.asList());
+
+            return equip;
+        }catch(Exception e) {
+            System.err.println(e.toString());
+            return null;
+        }
+    }
+
 }
