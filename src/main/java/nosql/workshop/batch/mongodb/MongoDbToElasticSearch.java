9c60a58e27cfaab762a01ed9aa7eb558b94a0a8b
Julie Bureau <juliebureau44@gmail.com>
2016-02-09 17:03:23 +0100

    MongoDbTiElasticSearch + csvVilles

--- src/main/java/nosql/workshop/batch/mongodb/MongoDbToElasticSearch.java
+++ src/main/java/nosql/workshop/batch/mongodb/MongoDbToElasticSearch.java
@@ -2,16 +2,11 @@ package nosql.workshop.batch.mongodb;
 
 import com.mongodb.*;
 import io.searchbox.client.JestClient;
-import io.searchbox.client.JestClientFactory;
-import io.searchbox.client.config.HttpClientConfig;
 import io.searchbox.core.Bulk;
-import io.searchbox.core.BulkResult;
 import io.searchbox.core.Index;
-import io.searchbox.indices.CreateIndex;
 import io.searchbox.indices.DeleteIndex;
 import io.searchbox.indices.IndicesExists;
-import nosql.workshop.model.Installation;
-import org.bson.types.ObjectId;
+import nosql.workshop.connection.ESConnectionUtil;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -23,52 +18,63 @@ import java.util.Map;
 public class MongoDbToElasticSearch {
 
     public static final String COL_INSTALLATIONS = "installations";
-    public static final String COL_EQUIPEMENTS = "equipements";
-    public static final String COL_ACTIVITES = "activites";
+
 
     public static void main(String[] args) {
-
-        String givenUri = System.getenv("MONGOLAB_URI");
-        String uri = givenUri == null ? "mongodb://localhost:27017/nosql-workshop" : givenUri;
-        MongoClientURI mongoClientURI = new MongoClientURI(uri);
-        MongoClient mongoClient = new MongoClient(mongoClientURI);
-        DB db = mongoClient.getDB(mongoClientURI.getDatabase());
-
-        DBCollection collection = db.getCollection(COL_INSTALLATIONS);
-
-        String connectionUrl = "http://localhost:9200";
-        JestClientFactory factory = new JestClientFactory();
-        factory.setHttpClientConfig(new HttpClientConfig
-                .Builder(connectionUrl)
-                .multiThreaded(true)
-                .build());
-        JestClient client = factory.getObject();
-
-        DBCursor cursor = collection.find();
-
-        Map<String, DBObject> source = new HashMap<String, DBObject>();
+        JestClient client = ESConnectionUtil.createClient("MONGOLAB_URI");
 
         try {
-            Bulk.Builder bulkIndexBuilder = new Bulk.Builder();
-            while(cursor.hasNext()) {
-                DBObject dbObject = cursor.next();
-                source.put(dbObject.get("_id").toString(), dbObject);
-            }
+            // Si la collection existe déjà, on la supprime
             boolean indexExists = client.execute(new IndicesExists.Builder(COL_INSTALLATIONS).build()).isSucceeded();
             if (indexExists) {
                 client.execute(new DeleteIndex.Builder(COL_INSTALLATIONS).build());
             }
-            bulkIndexBuilder.addAction(new Index.Builder(source)
-             .index(COL_INSTALLATIONS)
-             .type("installation")
-             .build());
-            client.execute(bulkIndexBuilder.build());
+
+            // Récupération des données en base MongoDb
+            Map<String, DBObject> source = listInstallations();
+
+            // Envoi sur la base ElasticSearch
+            Bulk bulk = new Bulk.Builder()
+                    .defaultIndex(COL_INSTALLATIONS)
+                    .defaultType("installation")
+                    .addAction(new Index.Builder(source).build())
+                    .build();
+
+            client.execute(bulk);
         } catch (IOException e) {
-        e.printStackTrace();
-        } finally {
-            cursor.close();
+            e.printStackTrace();
         }
     }
 
+
+    private static Map<String, DBObject> listInstallations() {
+        DBCollection collection = getDbCollection();
+
+        DBCursor cursor = collection.find();
+        cursor.addOption(Bytes.QUERYOPTION_NOTIMEOUT);
+
+        Map<String, DBObject> source = new HashMap<String, DBObject>();
+
+        // On récupère les 500 premiers objets
+        int i = 500;
+        while (cursor.hasNext() && i > 0) {
+            DBObject dbObject = cursor.next();
+            source.put(dbObject.get("_id").toString(), dbObject);
+            i--;
+        }
+        cursor.close();
+
+        return source;
+    }
+
+    private static DBCollection getDbCollection() {
+        String givenUri = System.getenv("MONGOLAB_URI");
+        String uri = givenUri == null ? "mongodb://localhost:27017/nosql-workshop" : givenUri;
+        MongoClientURI mongoClientURI = new MongoClientURI(uri);
+        MongoClient mongoClient = new MongoClient(mongoClientURI);
+
+        DB db = mongoClient.getDB(mongoClientURI.getDatabase());
+        return db.getCollection(COL_INSTALLATIONS);
+    }
 
 }
