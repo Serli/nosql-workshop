246c8dcff6c6186a58367e0481d5f98e7bf7e0c4
Joris <joris.44@live.fr>
2016-02-09 11:43:52 +0100

    Insert data into Elasticsearch

--- src/main/java/nosql/workshop/batch/elasticsearch/MongoDbToElasticsearch.java
+++ src/main/java/nosql/workshop/batch/elasticsearch/MongoDbToElasticsearch.java
@@ -1,46 +1,71 @@
 package nosql.workshop.batch.elasticsearch;
 
-import com.mongodb.Block;
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.*;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
-import nosql.workshop.model.Installation;
-import nosql.workshop.resources.InstallationResource;
-import org.bson.Document;
+import io.searchbox.core.Bulk;
+import io.searchbox.core.Index;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Joris on 09/02/2016.
  */
 public class MongoDbToElasticsearch {
 
-    public static void main(String[] args) {
+    private MongoClient mongoClient;
 
+    public MongoDbToElasticsearch() {
+        mongoClient = new MongoClient();
     }
 
-    public void readData(){
-        MongoClient mongoClient = new MongoClient();
-        MongoDatabase db = mongoClient.getDatabase("nosql-workshop");
-        FindIterable<Document> iterable = db.getCollection("installations").find();
+    public static void main(String[] args) {
+        MongoDbToElasticsearch mongoDbToElasticsearch = new MongoDbToElasticsearch();
+        mongoDbToElasticsearch.writeToElastic();
+    }
 
-        iterable.forEach(new Block<Document>() {
-            @Override
-            public void apply(final Document document) {
-                System.out.println(document);
-            }
-        });
+    public List<DBObject> getInstallations(DBCursor cursor){
+        List<DBObject> itemList = new ArrayList<>();
+
+        while(cursor.hasNext()){
+            itemList.add(cursor.next());
+        }
+
+        return itemList;
     }
 
     public void writeToElastic(){
-        String connectionUrl = "http://localhost";
+        DB db = mongoClient.getDB("nosql-workshop");
+        DBCollection col = db.getCollection("installation");
+        DBCursor cursor = col.find();
+
+        String connectionUrl = "http://localhost:9200";
         JestClientFactory factory = new JestClientFactory();
         factory.setHttpClientConfig(new HttpClientConfig
                 .Builder(connectionUrl)
                 .multiThreaded(true)
                 .build());
         JestClient client = factory.getObject();
+
+        Map<String, Object> source = new HashMap<>();
+        source.put("installation", this.getInstallations(cursor));
+
+        Bulk bulk = new Bulk.Builder()
+                .defaultIndex("installations")
+                .defaultType("installation")
+                .addAction(new Index.Builder(source).build())
+                .build();
+
+        try {
+            client.execute(bulk);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
