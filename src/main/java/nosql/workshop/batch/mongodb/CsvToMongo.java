819627cef772ebc83f3540b4c3a5e602e35eabf8
Hadrien Gerard <Hadrien Gerard>
2016-02-09 12:06:49 +0100

    CsvToMongo batch completed

--- src/main/java/nosql/workshop/batch/mongodb/CsvToMongo.java
+++ src/main/java/nosql/workshop/batch/mongodb/CsvToMongo.java
@@ -3,8 +3,6 @@ package nosql.workshop.batch.mongodb;
 import com.mongodb.*;
 import com.opencsv.CSVReader;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -30,6 +28,8 @@ public class CsvToMongo {
         CsvToMongo csvToMongo = new CsvToMongo();
         csvToMongo.dropDB();
         csvToMongo.installations();
+        csvToMongo.equipements();
+        csvToMongo.activities();
     }
 
     private void dropDB() {
@@ -84,94 +84,47 @@ public class CsvToMongo {
     }
 
     public void equipements() {
+        DB db = mongoClient.getDB("nosql-workshop");
+        DBCollection col = db.getCollection("installation");
+        List<String[]> myEntries = readCSV("src\\main\\resources\\batch\\csv\\equipements.csv");
+        myEntries.stream()
+                .skip(1)
+                .forEach(columns -> {
+                    DBObject installation = col.findOne(columns[2].trim());
+                    DBObject obj = new BasicDBObject()
+                            .append("numero", columns[4].trim())
+                            .append("nom", columns[5].trim())
+                            .append("type", columns[7].trim())
+                            .append("famille", columns[9].trim());
+                            //.append("activites", new BasicDBList());
 
+                    col.update(installation, new BasicDBObject("$push", new BasicDBObject("equipements", obj)));
+                });
     }
+
+    public void activities() {
+        DB db = mongoClient.getDB("nosql-workshop");
+        DBCollection col = db.getCollection("installation");
+        List<String[]> myEntries = readCSV("src\\main\\resources\\batch\\csv\\activites.csv");
+        myEntries.stream()
+                .skip(1)
+                .forEach(columns -> {
+                    DBObject queryForElem = new BasicDBObject("equipements", new BasicDBObject("$elemMatch", new BasicDBObject("numero", columns[2].trim())));
+                    DBObject updateMatchingElem = new BasicDBObject("$push", new BasicDBObject("equipements.$.activites", columns[5].trim()));
+                    col.update(queryForElem, updateMatchingElem);
+                });
+    }
+
 
     public static List<String[]> readCSV(String path) {
         CSVReader reader = null;
         try {
             reader = new CSVReader(new FileReader(path));
             return reader.readAll();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return new ArrayList<>();
-
-        /*
-          try (InputStream inputStream = CsvToMongo.class.getResourceAsStream("/batch/csv/installations.csv");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            reader.lines()
-                    .skip(1)
-                    .filter(line -> line.length() > 0)
-                    .map(line -> line.split(","))
-                    .forEach(columns -> {
-                        System.out.println("Une ligne");
-                        System.out.println(columns[0].matches("\".*\"")?columns[0].substring(1,columns[0].length()-1):columns[0]);
-                    });
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
-         */
-    }
-
-
-    public void activities() {
-
-        DB db = mongoClient.getDB("nosql-workshop");
-        DBCollection col = db.getCollection("activites");
-        List<String[]> myEntries = readCSV("src\\main\\resources\\batch\\csv\\activites.csv");
-
-
-            for (String[] columns : myEntries) {
-                System.out.println("ici :" + columns[9]);
-
-                DBObject obj = new BasicDBObject()
-                        .append("numero", columns[4])
-                        .append("nom", columns[5]);
-
-
-
-             /*   DBObject obj = new BasicDBObject()
-                        .append("_id", columns[1])
-                        .append("nom", columns[0])
-                        .append("adresse",
-                                new BasicDBObject()
-                                        .append("numero", columns[6])
-                                        .append("voie", columns[7])
-                                        .append("lieuDit", columns[5])
-                                        .append("codePostal", columns[4])
-                                        .append("commune", columns[2])
-                        )
-                        .append(
-                                "location",
-                                new BasicDBObject("type", "Point")
-                                        .append(
-                                                "coordinates",
-                                                Arrays.asList(
-                                                        Double.valueOf(columns[9]),
-                                                        Double.valueOf(columns[10])
-                                                )
-                                        )
-                        )
-                        .append("multiCommune", "Oui".equals(columns[16]))
-                        .append("nbPlacesParking", columns[17].isEmpty() ? null : Integer.valueOf(columns[17]))
-                        .append("nbPlacesParkingHandicapes", columns[18].isEmpty() ? null : Integer.valueOf(columns[18]))
-                        .append(
-                                "dateMiseAJourFiche",
-                                columns.length < 29 || columns[28].isEmpty()
-                                        ? null :
-                                        Date.from(
-                                                LocalDate.parse(columns[28].substring(0, 10))
-                                                        .atStartOfDay(ZoneId.of("UTC"))
-                                                        .toInstant()
-                                        )
-                        );*/
-
-
-            }
-
     }
 
 }
