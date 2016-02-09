f319fe2010b908a4a62b24d0ffdd3c9eac40bb59
Marion Bechennec <marion@dictanova.com>
2016-02-09 08:51:50 +0100

    Using java 8 api Stream to read CSV

--- src/main/java/nosql/workshop/batch/mongodb/ImportDataInMongo.java
+++ src/main/java/nosql/workshop/batch/mongodb/ImportDataInMongo.java
@@ -1,35 +1,40 @@
 package nosql.workshop.batch.mongodb;
 
-import org.apache.commons.csv.CSVFormat;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 
 /**
  * @author Marion Bechennec
  */
 public class ImportDataInMongo {
 
-    private static final String[] INSTALLATION_HEADER = {"Nom usuel de l'installation","Numéro de l'installation","Nom de la commune","Code INSEE","Code postal","Nom du lieu dit","Numero de la voie","Nom de la voie","location","Longitude","Latitude","Aucun aménagement d'accessibilité","Accessibilité handicapés à mobilité réduite","Accessibilité handicapés sensoriels","Emprise foncière en m2","Gardiennée avec ou sans logement de gardien","Multi commune","Nombre total de place de parking","Nombre total de place de parking handicapés","Installation particulière","Desserte métro","Desserte bus","Desserte Tram","Desserte train","Desserte bateau","Desserte autre","Nombre total d'équipements sportifs","Nombre total de fiches équipements","Date de mise à jour de la fiche installation"};
-
     public static void main(String[] args) {
-        //insert installation
+        //insert installations
         ImportDataInMongo importer = new ImportDataInMongo();
 
-        importer.installation();
-        //update installation with equipement
-        //update equipement in installation with activites
+        importer.insertInstallations();
+        //update installations with equipement
+        importer.updateEquipements();
+        //update equipement in insertInstallations with activites
+        importer.updateActivites();
 
     }
 
-    private void installation() {
-        try {
-            FileReader fileReader = new FileReader("activites.csv");
-            CSVFormat csvFileFormat = CSVFormat.DEFAULT.withHeader(INSTALLATION_HEADER);
+    private void insertInstallations() {
+        InputStream is = getClass().getResourceAsStream("installations.csv");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
+        br.lines()
+            .skip(1)
+            .filter(line -> line.length() > 0)
+            .map(line -> line.split(","))
+            .forEach(column -> {
+                //TODO insert in mongo
+            });
+    }
 
+    private void updateEquipements() {
+    }
 
-        } catch (FileNotFoundException e) {
-        }
+    private void updateActivites() {
     }
 }
