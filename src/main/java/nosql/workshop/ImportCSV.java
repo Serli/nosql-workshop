63be3185c69a1c7d987486edb9c6c09fee5a1df5
Augustin De Vita <augustin.devita@gmail.com>
2016-02-09 10:39:25 +0100

    MongoDB installations et equiments faits

--- src/main/java/nosql/workshop/ImportCSV.java
+++ src/main/java/nosql/workshop/ImportCSV.java
@@ -39,7 +39,7 @@ public class ImportCSV {
 		String csvFile = "src/main/resources/batch/csv/installations.csv";
 		BufferedReader br = null;
 		String line = "";
-		String cvsSplitBy = ",";
+		String cvsSplitBy = "\"";
 
 		try {
 
@@ -53,40 +53,40 @@ public class ImportCSV {
 
 				// use comma as separator
 				String[] data = line.split(cvsSplitBy);
-
+				//System.out.println(data.length);
 				//Nom usuel de l'installation0,"NumÃ©ro de l'installation"1,"Nom de la commune"2,"Code INSEE"3,"Code postal"4,"Nom du lieu dit"5,"Numero de la voie"6,"Nom de la voie"7,"location"8,"Longitude"9,"Latitude"10,"Aucun amÃ©nagement d'accessibilitÃ©"11,"AccessibilitÃ© handicapÃ©s Ã  mobilitÃ© rÃ©duite"12,"AccessibilitÃ© handicapÃ©s sensoriels"13,"Emprise fonciÃ¨re en m2"14,"GardiennÃ©e avec ou sans logement de gardien"15,"Multi commune"16,"Nombre total de place de parking"17,"Nombre total de place de parking handicapÃ©s"18,"Installation particuliÃ¨re"19,"Desserte mÃ©tro"19,"Desserte bus"20,"Desserte Tram"21,"Desserte train"22,"Desserte bateau"23,"Desserte autre"24,"Nombre total d'Ã©quipements sportifs"25,"Nombre total de fiches Ã©quipements"26,"Date de mise Ã  jour de la fiche installation"27
 				inst = new BasicDBObject()
-						.append("_id", data[1])
-						.append("nom", data[0])
+						.append("_id", data[3])
+						.append("nom", data[1])
 						.append("adresse", new BasicDBObject()
-								.append("numero", data[6])
-								.append("voie", data[7])
-								.append("lieuDit", data[5])
-								.append("codePostal", data[4])
-								.append("commune", data[2])
+								.append("numero", data[13])
+								.append("voie", data[15])
+								.append("lieuDit", data[11])
+								.append("codePostal", data[9])
+								.append("commune", data[5])
 								)
 						.append("location", new BasicDBObject()
 								.append("type", "point")
-								.append("coordinates",  Arrays.asList(Double.valueOf(data[9]), Double.valueOf(data[10]))))
-						.append("multiCommune", "Oui".equals(data[16]))
-						.append("nbPlacesParking", data[17].isEmpty() ? null : Integer.valueOf(data[17]))
-						.append("nbPlacesParkingHandicapes", data[18].isEmpty() ? null : Integer.valueOf(data[18]))
+								.append("coordinates",  Arrays.asList(Double.parseDouble(data[19]), Double.parseDouble(data[21])))
+						)
+						.append("multiCommune", "Oui".equals((data[35])))
+						.append("nbPlacesParking", data[35].isEmpty() ? null : Integer.valueOf(data[35]))
+						.append("nbPlacesParkingHandicapes", data[37].isEmpty() ? null : Integer.valueOf(data[37]))
 						.append(
 								"dateMiseAJourFiche",
-								data.length < 29 || data[28].isEmpty()
+								data.length < 58 || data[57].isEmpty()
 								? null :
 									Date.from(
-											LocalDate.parse(data[28].substring(0, 10))
+											LocalDate.parse(data[57].substring(0, 10))
 											.atStartOfDay(ZoneId.of("UTC"))
 											.toInstant()
 											)
 								);
 
-
+				installations.insert(inst);
 
 
 			}
-			installations.insert(inst);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -101,6 +101,7 @@ public class ImportCSV {
 			}
 		}
 	}
+	
 
 	public static void importEquipement(DBCollection equipements) {
 
@@ -129,12 +130,12 @@ public class ImportCSV {
 						.append("type", data[7]) // EquipemementTypeLib
 						.append("famille", data[9]); // FamilleFicheLib
 
+				equipements.insert(equipement);
 
 			}
 
 
 
-			equipements.insert(equipement);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
