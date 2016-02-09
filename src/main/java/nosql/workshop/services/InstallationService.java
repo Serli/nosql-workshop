612bb595094c9e8fe941db851e9d64d072a94002
Jorpheus7 <jordan.gobley.pro@gmail.com>
2016-02-09 14:02:13 +0100

    Setting the methods for service

--- src/main/java/nosql/workshop/services/InstallationService.java
+++ src/main/java/nosql/workshop/services/InstallationService.java
@@ -1,6 +1,10 @@
 package nosql.workshop.services;
 
 import com.google.inject.Singleton;
+import nosql.workshop.model.Installation;
+import nosql.workshop.model.stats.InstallationsStats;
+
+import java.util.List;
 
 /**
  * Service permettant de manipuler les installations sportives.
@@ -8,4 +12,27 @@ import com.google.inject.Singleton;
 @Singleton
 public class InstallationService {
 
+    public List<Installation> getAllInstallations(){
+        return null;
+    }
+
+    public Installation getInstallation(Double numero){
+        return null;
+    }
+
+    public List<Installation> searchInstallations(){
+        return null;
+    }
+
+    public Installation getRandomInstallation(){
+        return null;
+    }
+
+    public List<Installation> getInstallationByGeoSearch(){
+        return null;
+    }
+
+    public InstallationsStats getStats(){
+        return null;
+    }
 }
