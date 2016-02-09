03716eebe149cbcb13bbf2404c156609f17f560c
Jorpheus7 <jordan.gobley.pro@gmail.com>
2016-02-09 09:19:59 +0100

    Inserting installation data into mongo using java driver

--- src/main/java/nosql/workshop/batch/mongodb/ImportDataInMongo.java
+++ src/main/java/nosql/workshop/batch/mongodb/ImportDataInMongo.java
@@ -1,5 +1,7 @@
 package nosql.workshop.batch.mongodb;
 
+import com.mongodb.*;
+
 import java.io.*;
 
 /**
@@ -7,11 +9,16 @@ import java.io.*;
  */
 public class ImportDataInMongo {
 
+    private static final String DB_NAME = "nosql-workshop";
+
     public static void main(String[] args) {
         //insert installations
         ImportDataInMongo importer = new ImportDataInMongo();
 
-        importer.insertInstallations();
+        MongoClient mongoClient = new MongoClient();
+        DB db = mongoClient.getDB(DB_NAME);
+
+        importer.insertInstallations(db);
         //update installations with equipement
         importer.updateEquipements();
         //update equipement in insertInstallations with activites
@@ -19,7 +26,9 @@ public class ImportDataInMongo {
 
     }
 
-    private void insertInstallations() {
+    private void insertInstallations(DB db) {
+        DBCollection col = db.getCollection("installations");
+
         InputStream is = getClass().getResourceAsStream("installations.csv");
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
@@ -29,6 +38,22 @@ public class ImportDataInMongo {
             .map(line -> line.split(","))
             .forEach(column -> {
                 //TODO insert in mongo
+                DBObject installation = new BasicDBObject("type", "Installation")
+                        .append("_id", column[1])
+                        .append("nom", column[0])
+                        .append("adresse", new BasicDBObject("type", "adresse")
+                                .append("numero", column[6])
+                                .append("voie", column[7])
+                                .append("lieuDit", column[5])
+                                .append("codePostal", column[4])
+                                .append("commune", column[2]))
+                        .append("location", new BasicDBObject("type", "Point")
+                                .append("coordinates", column[8]))
+                        .append("multiCommune", column[16])
+                        .append("nbPlacesParking", column[17])
+                        .append("nbPlacesParkingHandicapes", column[18])
+                        .append("dateMiseAJourFiche", column[28]);
+                col.insert(installation);
             });
     }
 
@@ -37,4 +62,5 @@ public class ImportDataInMongo {
 
     private void updateActivites() {
     }
+
 }
