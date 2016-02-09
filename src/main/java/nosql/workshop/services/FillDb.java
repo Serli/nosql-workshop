43e24e65893c5366e6da97dff0b275a8ecadded2
vers-a <vers.angelin@gmail.com>
2016-02-09 08:49:16 +0100

    write logic of fillWithCsv

--- src/main/java/nosql/workshop/services/FillDb.java
+++ src/main/java/nosql/workshop/services/FillDb.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
