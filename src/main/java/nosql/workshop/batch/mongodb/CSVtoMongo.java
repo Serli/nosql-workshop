e202e8f5529a2998d48db96844b4f5da7ae10be1
dams53 <damsplaystation@msn.com>
2016-02-09 17:29:15 +0100

    Refactor CVStoMongo

--- src/main/java/nosql/workshop/batch/mongodb/CSVtoMongo.java
+++ src/main/java/nosql/workshop/batch/mongodb/CSVtoMongo.java
@@ -32,64 +32,69 @@ public class CSVtoMongo {
 
         collection.drop();
 
-    for (Map<String, String> line : ReadCVS.run(pathInstallation)) {
-        DBObject document = new BasicDBObject()
-                .append("_id", line.get("Numéro de l'installation"))
-                .append("nom", line.get("Nom usuel de l'installation"))
-                .append("adresse",
-                        new BasicDBObject()
-                                .append("numero", line.get("Numero de la voie"))
-                                .append("voie", line.get("Nom de la voie"))
-                                .append("lieuDit", line.get("Nom du lieu dit"))
-                                .append("codePostal", line.get("Code postal"))
-                                .append("commune", line.get("Nom de la commune"))
-                )
-                .append(
-                        "location",
-                        new BasicDBObject("type", "Point")
-                                .append(
-                                        "coordinates",
-                                        Arrays.asList(
-                                                Double.valueOf(line.get("Longitude")),
-                                                Double.valueOf(line.get("Latitude"))
-                                        )
-                                )
-                )
-                .append("multiCommune", "Oui".equals(line.get("Multi commune")))
-                .append("nbPlacesParking", line.get("Nombre total de place de parking") == null || line.get("Nombre total de place de parking").isEmpty() ? 0 : Integer.valueOf(line.get("Nombre total de place de parking")))
-                .append("nbPlacesParkingHandicapes", line.get("Nombre total de place de parking handicapés") == null || line.get("Nombre total de place de parking handicapés").isEmpty() ? 0 : Integer.valueOf(line.get("Nombre total de place de parking handicapés")))
-                .append(
-                        "dateMiseAJourFiche",
-                        line.get("Date de mise à jour de la fiche installation") == null || line.get("Date de mise à jour de la fiche installation").isEmpty()
-                                ? null :
-                                Date.from(
-                                        LocalDate.parse(line.get("Date de mise à jour de la fiche installation").substring(0, 10))
-                                                .atStartOfDay(ZoneId.of("UTC"))
-                                                .toInstant()
-                                )
-                );
-        collection.insert(document);
+        remplissageInstallations(collection);
+        remplissageEquipements(collection);
+        remplissageActivites(collection);
     }
 
-       for (Map<String, String> line : ReadCVS.run(pathEquipements)) {
+    public static void remplissageInstallations(DBCollection collection){
+        for (Map<String, String> line : ReadCVS.run(pathInstallation)) {
+            DBObject document = new BasicDBObject()
+                    .append("_id", line.get("Numéro de l'installation"))
+                    .append("nom", line.get("Nom usuel de l'installation"))
+                    .append("adresse",
+                            new BasicDBObject()
+                                    .append("numero", line.get("Numero de la voie"))
+                                    .append("voie", line.get("Nom de la voie"))
+                                    .append("lieuDit", line.get("Nom du lieu dit"))
+                                    .append("codePostal", line.get("Code postal"))
+                                    .append("commune", line.get("Nom de la commune"))
+                    )
+                    .append(
+                            "location",
+                            new BasicDBObject("type", "Point")
+                                    .append(
+                                            "coordinates",
+                                            Arrays.asList(
+                                                    Double.valueOf(line.get("Longitude")),
+                                                    Double.valueOf(line.get("Latitude"))
+                                            )
+                                    )
+                    )
+                    .append("multiCommune", "Oui".equals(line.get("Multi commune")))
+                    .append("nbPlacesParking", line.get("Nombre total de place de parking") == null || line.get("Nombre total de place de parking").isEmpty() ? 0 : Integer.valueOf(line.get("Nombre total de place de parking")))
+                    .append("nbPlacesParkingHandicapes", line.get("Nombre total de place de parking handicapés") == null || line.get("Nombre total de place de parking handicapés").isEmpty() ? 0 : Integer.valueOf(line.get("Nombre total de place de parking handicapés")))
+                    .append(
+                            "dateMiseAJourFiche",
+                            line.get("Date de mise à jour de la fiche installation") == null || line.get("Date de mise à jour de la fiche installation").isEmpty()
+                                    ? null :
+                                    Date.from(
+                                            LocalDate.parse(line.get("Date de mise à jour de la fiche installation").substring(0, 10))
+                                                    .atStartOfDay(ZoneId.of("UTC"))
+                                                    .toInstant()
+                                    )
+                    );
+            collection.insert(document);
+        }
+    }
+
+    public static void remplissageEquipements(DBCollection collection){
+        for (Map<String, String> line : ReadCVS.run(pathEquipements)) {
             collection.update(collection.findOne(line.get("InsNumeroInstall")),
                     new BasicDBObject("$push",new BasicDBObject("equipements", new BasicDBObject()
-                    .append("numero", line.get("EquipementId"))
-                    .append("nom", line.get("EquNom"))
-                    .append("type", line.get("EquipementTypeLib"))
-                    .append("famille", line.get("FamilleFicheLib")))));
+                            .append("numero", line.get("EquipementId"))
+                            .append("nom", line.get("EquNom"))
+                            .append("type", line.get("EquipementTypeLib"))
+                            .append("famille", line.get("FamilleFicheLib")))));
         }
-
-       for (Map<String, String> line : ReadCVS.run(pathActivites)) {
-           BasicDBObject searchQuery = new BasicDBObject("equipements", new BasicDBObject("$elemMatch", new BasicDBObject("numero", line.get("Numéro de la fiche équipement").trim())));
-           BasicDBObject updateQuery = new BasicDBObject("$push", new BasicDBObject("equipements.$.activites", line.get("Activité libellé")));
-           collection.update(searchQuery, updateQuery);
-        }
-
     }
 
-
-
-
+    public static void remplissageActivites(DBCollection collection){
+        for (Map<String, String> line : ReadCVS.run(pathActivites)) {
+            BasicDBObject searchQuery = new BasicDBObject("equipements", new BasicDBObject("$elemMatch", new BasicDBObject("numero", line.get("Numéro de la fiche équipement").trim())));
+            BasicDBObject updateQuery = new BasicDBObject("$push", new BasicDBObject("equipements.$.activites", line.get("Activité libellé")));
+            collection.update(searchQuery, updateQuery);
+        }
+    }
 
 }
