3c94eee6005984370b51830019c521f6ec2bcf19
Julie Bureau <juliebureau44@gmail.com>
2016-02-10 11:16:41 +0100

    get location

--- src/main/java/nosql/workshop/services/SearchService.java
+++ src/main/java/nosql/workshop/services/SearchService.java
@@ -3,6 +3,8 @@ package nosql.workshop.services;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
 import io.searchbox.core.Search;
 import io.searchbox.core.SearchResult;
 import io.searchbox.core.Suggest;
@@ -10,16 +12,27 @@ import io.searchbox.core.SuggestResult;
 import nosql.workshop.connection.ESConnectionUtil;
 import nosql.workshop.model.Installation;
 import nosql.workshop.model.suggest.TownSuggest;
+import org.elasticsearch.action.suggest.SuggestRequestBuilder;
+import org.elasticsearch.action.suggest.SuggestResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.suggest.completion.CompletionSuggestion;
+import org.elasticsearch.search.suggest.completion.CompletionSuggestionBuilder;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Search service permet d'encapsuler les appels vers ElasticSearch
@@ -32,8 +45,70 @@ public class SearchService {
 
     }
 
-    public List<TownSuggest> suggest(String text) {
-        return null;
+    public List<TownSuggest> suggest(String text) throws IOException {
+
+        JestClient client = ESConnectionUtil.createClient("MONGOLAB_URI");
+
+        String query2 = "{\n"
+                + "    \"query\": {\n"
+                + "        \"filtered\" : {\n"
+                + "            \"query\" : {\n"
+                + "                \"query_string\" : {\n"
+                + "                    \"query\" : \"java\"\n"
+                + "                }\n"
+                + "            }"
+                + "        }\n"
+                + "    }\n"
+                + "}";
+
+        String query = "{\"suggest\" : {\n"
+                + "         \"town-suggest\" : {\n"
+                + "             \"text\" : \"n\",\n"
+                + "             \"term\" : {\n"
+                + "                 \"field\" : \"townName\"\n"
+                + "             }\n"
+                + "         }"
+                + "     }}";
+
+
+       //Suggest suggest = new Suggest.Builder(query, "suggest").addIndex("towns").build();
+        Search.Builder searchBuilder = new Search.Builder(query).addIndex("towns");
+
+
+        SearchResult result = client.execute(searchBuilder.build());
+        System.out.println("suggestResult");
+
+        List<SearchResult.Hit<TownSuggest, Void>> hits = result.getHits(TownSuggest.class);
+        List<TownSuggest> towns = new ArrayList<TownSuggest>();
+        if (!hits.isEmpty()) {
+            for (SearchResult.Hit<TownSuggest, Void> hit : hits) {
+                towns.add(hit.source);
+            }
+        }
+        return towns;
     }
 
+    public Double[] getLocation(String townName) throws IOException {
+
+        JestClient client = ESConnectionUtil.createClient("MONGOLAB_URI");
+
+        String query = "{\"query\": {\n"
+            + "     \"match\": {\n"
+            + "         \"townName\": \"" + townName + "\"\n"
+            + "     }\n"
+            + "}}";
+
+
+        Search.Builder searchBuilder = new Search.Builder(query).addIndex("towns").addType("town");
+
+        SearchResult result = client.execute(searchBuilder.build());
+        List<SearchResult.Hit<TownSuggest, Void>> hits = result.getHits(TownSuggest.class);
+        if (!hits.isEmpty()) {
+            SearchResult.Hit<TownSuggest, Void> townHit = hits.get(0);
+            return townHit.source.getLocation();
+        }
+        else {
+            return null;
+        }
+    }
 }
