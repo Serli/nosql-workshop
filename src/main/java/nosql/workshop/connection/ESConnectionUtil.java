f8634a79c3a5d3dd63716b25f2bf1abc97391df6
Maxime P <max.peroch@hotmail.fr>
2016-02-10 12:00:54 +0100

    increased timedout + added mapping fautes de frappes

--- src/main/java/nosql/workshop/connection/ESConnectionUtil.java
+++ src/main/java/nosql/workshop/connection/ESConnectionUtil.java
@@ -18,6 +18,7 @@ public abstract class ESConnectionUtil {
         JestClientFactory factory = new JestClientFactory();
         factory.setHttpClientConfig(new HttpClientConfig
                 .Builder(serverUri)
+                .readTimeout(600000)
                 .multiThreaded(true)
                 .build());
         return factory.getObject();
