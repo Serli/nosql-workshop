8fe593aef1e1db543d86c7c5fe62a60ee42c16ad
Xartok <lagache.yoann@gmail.com>
2016-02-10 10:23:20 +0100

    Add batch to import mongo data in elasticsearch

---
+++ src/main/java/nosql/workshop/batch/elasticsearch/BatchMongoElastic.java
@@ -0,0 +1,40 @@
+package nosql.workshop.batch.elasticsearch;
+
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Bulk;
+import io.searchbox.core.Index;
+import nosql.workshop.connection.ESConnectionUtil;
+
+import java.io.IOException;
+
+public class BatchMongoElastic {
+    public static void main(String[] args) {
+        JestClient jest = ESConnectionUtil.createClient("");
+        MongoClient mongoClient = new MongoClient();
+        DBCursor installations = mongoClient.getDB("nosql-workshop").getCollection("installations").find();
+
+        String index = "installations";
+        String type = "installation";
+        Bulk.Builder builder = new Bulk.Builder();
+        installations.forEach(installation -> {
+            builder.addAction(createIndex(installation, index, type));
+        });
+        Bulk bulk = builder.build();
+
+        try {
+            jest.execute(bulk);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Index createIndex(DBObject installation, String index, String type) {
+        String id = installation.get("_id").toString();
+        installation.removeField("_id");
+        installation.removeField("dateMiseAJourFiche");
+        return new Index.Builder(installation).index(index).type(type).id(id).build();
+    }
+}
