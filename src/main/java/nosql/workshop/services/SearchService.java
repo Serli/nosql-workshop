28c362a13bc6eea7c0e0c56c04d859ded4857834
Marion Bechennec <marion@dictanova.com>
2016-02-10 13:19:27 +0100

    optimizing imports

--- src/main/java/nosql/workshop/services/SearchService.java
+++ src/main/java/nosql/workshop/services/SearchService.java
@@ -1,28 +1,17 @@
 package nosql.workshop.services;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestResult;
 import io.searchbox.core.Search;
-import io.searchbox.core.SearchResult;
-import io.searchbox.core.Suggest;
-import io.searchbox.core.SuggestResult;
 import nosql.workshop.connection.ESConnectionUtil;
-import nosql.workshop.model.Installation;
 import nosql.workshop.model.suggest.TownSuggest;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Search service permet d'encapsuler les appels vers ElasticSearch
