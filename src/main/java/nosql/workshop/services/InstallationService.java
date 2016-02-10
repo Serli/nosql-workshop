b5bf25558247458479e9a64667a624af4ec373f6
EMNico <boulangernicolas.emn@gmail.com>
2016-02-10 10:43:05 +0100

    stats

--- src/main/java/nosql/workshop/services/InstallationService.java
+++ src/main/java/nosql/workshop/services/InstallationService.java
@@ -1,5 +1,6 @@
 package nosql.workshop.services;
 
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.mongodb.BasicDBObject;
@@ -7,6 +8,7 @@ import com.mongodb.BasicDBObject;
 import net.codestory.http.Context;
 import nosql.workshop.model.Equipement;
 import nosql.workshop.model.Installation;
+import nosql.workshop.model.stats.CountByActivity;
 import nosql.workshop.model.stats.InstallationsStats;
 
 import org.jongo.Find;
@@ -17,6 +19,7 @@ import org.omg.CORBA.SystemException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -73,7 +76,44 @@ public class InstallationService {
     
     public InstallationsStats stats(){
     	int totalCount = (int) installations.count();
-    	installations.aggregate("[{$group : {_id : \"$nbequ\", total : {$sum : 1}}}]");
-    	return null;
+    	// installations.aggregate("[{$project: { equipements: 1}} $group: { _id: \"$department\",average: { $avg: \"$sum\" } }]").as(Installation.class);
+    	
+    	double avg_equ = 0;
+    	int max_equ = 0;
+    	
+    	ArrayList<Installation> list = new ArrayList<Installation>();
+    	
+    	Iterator<CountByActivity> countbyActivitiesIt = installations.aggregate(
+				"{$match: {\"equipements.activites\":{$exists : true}}}")
+				.and("{$unwind: \"$equipements\"}")
+				.and("{$unwind: \"$equipements.activites\"}")
+				.and("{$group: {_id:null,activite:{'$first'unsure emoticon\"$equipements.activites\"}, total: {$sum:1}}}")
+				.and("{$sort: {total: -1}}").as(CountByActivity.class);
+    	ArrayList<CountByActivity> activities = Lists.newArrayList(countbyActivitiesIt);
+    	
+    	Consumer<Installation> action = (Installation s) -> list.add(s);
+    	installations.find().as(Installation.class).forEach(action);
+    	
+    	for (int i = 0 ; i < list.size() ; i++){
+    		avg_equ += list.get(i).getEquipements().size();
+    		if (list.get(i).getEquipements().size() > max_equ){
+    			max_equ = list.get(i).getEquipements().size();
+    		}
+    		/*for (Equipement equ : list.get(i).getEquipements()){
+    			for (String a : equ.getActivites()){
+    				
+    			}
+    		}*/
+    	}
+    	avg_equ = avg_equ/totalCount;
+    	
+    	Installation max_install = installations.findOne(String.format("{_nbequ: '%s'}", max_equ)).as(Installation.class);
+    	
+    	InstallationsStats stats = new InstallationsStats();
+    	stats.setAverageEquipmentsPerInstallation(avg_equ);
+    	stats.setTotalCount(totalCount);
+    	stats.setInstallationWithMaxEquipments(max_install);
+    	stats.setCountByActivity(activities);
+    	return stats;
     }
 }
